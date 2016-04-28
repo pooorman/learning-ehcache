e3d5ccde028262b2b9a3c493e9f1bb9e0a9e6c6b
Jianghan <jianghan@xiaomi.com>
2016-04-28 20:55:23 +0800

    First commit: sample cases

---
+++ learing-ehcache3.0/src/main/java/hahn/learning/ehcache/HeapCache.java
@@ -0,0 +1,32 @@
+package hahn.learning.ehcache;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
+/**
+ * Created by jianghan on 16-4-28.
+ */
+public class HeapCache {
+    public static void main(String[] args) throws Exception {
+        CacheManager manager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("preConfigured", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, byte[].class, ResourcePoolsBuilder.heap(10)))
+                .build();
+        manager.init();
+
+        Cache<String, byte[]> preConfigured = manager.getCache("preConfigured", String.class, byte[].class);
+
+        Cache<String, byte[]> myCache = manager
+                .createCache("myCache", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, byte[].class,
+                        ResourcePoolsBuilder.heap(10).offheap(10, MemoryUnit.MB)).build());
+
+        myCache.put("KEY_1", new byte[1024]);
+        byte[] value = myCache.get("KEY_1");
+        System.out.println(value.length);
+
+        manager.removeCache("preConfigured");
+        manager.close();
+    }
+}
